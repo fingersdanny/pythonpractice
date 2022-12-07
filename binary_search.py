4d54d26dd7823374a0a8019684f5635e8c5e1a3d
isevou__ <54776553+fingersdanny@users.noreply.github.com>
2022-12-08 00:47:37 +0900

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+##재귀적으로 구현(recursive)
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    if array[mid] == target:
+        return mid 
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+##반복문으로 구현(iterative)  
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
